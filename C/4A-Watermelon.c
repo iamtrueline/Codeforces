8f3634fb499f466baab51f4cb30b8aa8b4b26a63
iamtrueline <79238023+iamtrueline@users.noreply.github.com>
2021-05-31 14:51:20 +0900

    Create 4A-Watermelon.c

---
+++ C/4A-Watermelon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main (){
+    int a;
+    scanf("%d", &a);
+    if(a % 2 == 0){
+        if(a == 2){
+            printf("NO");
+        }else{
+            printf("YES");
+        }
+    }
+    else {
+        printf("NO");
+    }
+    return 0;
+}
